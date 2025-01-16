f7924cea84b33a3c4c4ed3518294b415f8b11308
이고은 <goeun26@sju.ac.kr>
2025-01-16 12:47:53 +0900

    DB URI삭제

--- index.js
+++ index.js
@@ -9,7 +9,7 @@ app.use(express.json());
 app.use("/user",UserRouter);
 app.use("/auth",AuthRouter);
 
-mongoose.connect("mongodb+srv://goeun79624:7btob1004%21@cluster0.skn21.mongodb.net/Arom?retryWrites=true&w=majority");
+mongoose.connect("");
 
 app.listen(3000,()=>{
     console.log("서버 실행됨")
